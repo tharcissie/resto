597ed8bff0e044b5578edd1494d23e1b9a2d854f
tharcissie <tharcissieidufashe@gmail.com>
2019-04-30 01:08:56 -0700

    second commit

--- app/src/main/java/com/example/teacher/resto/Main2Activity.java
+++ app/src/main/java/com/example/teacher/resto/Main2Activity.java
@@ -1,5 +1,8 @@
 package com.example.teacher.resto;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,11 +11,19 @@ public class Main2Activity extends AppCompatActivity {
 
 
      Toolbar tool;
+     DrawerLayout drawer;
+     NavigationView navigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer);
         tool= (Toolbar) findViewById(R.id.toolbar2);
-
+        setSupportActionBar(tool);
+        drawer=(DrawerLayout)findViewById(R.id.drawer);
+        navigation=(NavigationView)findViewById(R.id.navigation);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, drawer, tool,R.string.open_drawer,R.string.close_drawer);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
     }
 }
